dbaaed6e146a5639f46c824164058877baa47356
Sotos2013 <www.sotihatzi@gmail.com>
2023-12-11 19:53:42 +0200

    gg

--- www/index.php
+++ www/index.php
@@ -1,5 +1,19 @@
 
-<?php include_once "header.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/ludo.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="js/ludo.js" ></script>
+    <script src="js/ludo.js", src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+</head>
+<body>
 <div id="h1Name"><h1 id="name">LUDO</h1></div>
     <section id='main_body'>
         <div id='ludo_board'></div>
